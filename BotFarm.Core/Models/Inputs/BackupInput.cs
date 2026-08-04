b5fc737aa58a23e74435d3bc617fb6972b5d688a
ke4a <welder.sit-0q@icloud.com>
2026-08-04 15:08:27 +0300

    Remove unused classes

--- BotFarm.Core/Models/Inputs/BackupInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace BotFarm.Core.Models.Inputs;
-
-public class BackupInput
-{
-    [JsonPropertyName("name")]
-    public string Name { get; set; }
-}
